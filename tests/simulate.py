f801d25ff1ec1589ea970ce3a4b774aa490574cd
SukritDeb <debsukrit00@gmail.com>
2026-03-11 21:32:30 +0530

    hehe

--- tests/simulate.py
+++ tests/simulate.py
@@ -5,10 +5,10 @@ BASE_URL = "http://127.0.0.1:8000"
 
 print("Step 1: Sending 10 Normal Requests...")
 for _ in range(10):
-    requests.get(BASE_URL) # Small size, 1s gap
+    requests.get(BASE_URL)
     time.sleep(1)
 
 print("Step 2: Sending a 'Ghost' Anomaly (Large payload, zero gap)...")
-requests.post(BASE_URL, data="HACK"*250) # 1000 bytes
+requests.post(BASE_URL, data="HACK"*250) 
 
 print(f"Step 3: Check http://127.0.0.1:8000/analyze to see the -1 Noise point!")
